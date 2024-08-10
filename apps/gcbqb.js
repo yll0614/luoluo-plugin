483e5819cde17d82657a67cdb1519de452c26c59
心愿XY <2771424768@qq.com>
2024-08-10 21:13:01 +0000

    不再返回单一表情包，可以通过指令不同返回表情包 url暂时未添加完成，因为太困辣！
    Signed-off-by: 心愿XY <2771424768@qq.com>

--- apps/gcbqb.js
+++ apps/gcbqb.js
@@ -1,6 +1,6 @@
 // 引入必要的模块
-import fs from 'fs'
-import plugin from '../../../lib/plugins/plugin.js'
+import plugin from '../../../lib/plugins/plugin.js';
+import fetch from "node-fetch";
 import { Plugin_Name } from '../components/index.js'
 
 
@@ -21,7 +21,7 @@ export class gcbqb extends plugin {
             rule: [
                 {
                     /***匹配正则***/
-                    reg: '^[#/]?甘城(猫猫)?表情包$',
+                    reg: /^#?(.+)表情包$/,
                     /***执行方式***/
                     fnc: 'gcbqb'
                 }
@@ -31,15 +31,25 @@ export class gcbqb extends plugin {
     }
     /***甘城猫猫表情包***/
     async gcbqb(e) {
+        const type = e.msg.match(/^#?(.+)表情包$/)[1];//通过发送不同来获取不同表情包
+        const api = {
+            甘城猫猫: 'https://yugan.love?name=猫羽雫',
+            fufu: 'https://yugan.love?name=fufu'
+        };
+        //检查是否有对应url
+        if (!api[type]) {
+            await this.e.reply(`无法找到${type}的表情包，支持的表情包有'fufu,丛雨,甘城猫猫表情包类型,小南梁,千恋万花,古拉,心海,柴郡猫,满穗,猫猫虫,纳西妲,诗歌剧,龙图,kemomimi'`, true);
+            return;
+        }
         // 读取API配置文件
-        let pluginName = 'luoluo-plugin'; // 确定插件名称
-        let filePath = `./plugins/${pluginName}/config/AllAPI.json`;
-        let data = await fs.readFileSync(filePath);
-        const API = JSON.parse(data)
+        //let pluginName = 'luoluo-plugin'; // 假设这是你从某处获取的插件名称
+        //let filePath = `./plugins/${pluginName}/config/AllAPI.json`;
+        //let data = await fs.readFileSync(filePath);
+        //const API = JSON.parse(data)
         //直接使用api，不需要其他参数
-        let api = API.api5.url;
+        //let api = API.api5.url;
         /***发送消息***/
-        e.reply([segment.image(`${api}`)])
+        await e.reply(segment.image(api[type]));
         /***阻止消息继续往下***/
         return true
     }
