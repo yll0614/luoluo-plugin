5460420b92c6fab0b295543d107e68a22cd68a86
hao5520 <2771424768@qq.com>
2024-08-12 00:28:52 +0800

    添加api和表情包帮助

--- config/system/help_system.js
+++ config/system/help_system.js
@@ -32,7 +32,8 @@ export const helpList = [
       icon: 88,
       title: "#一言",
       desc: "顾名思义 就是一言啦"
-    },{
+    },
+    {
       icon: 63,
       title: "#网易云热评",
       desc: "顾名思义 就是网易云热评啦"
@@ -136,6 +137,103 @@ export const helpList = [
     }
   ]
 }
+]
+
+export const bqbhelpCfg = {
+  title: '表情包帮助',
+  subTitle: 'Yunzai-Bot & luoluo-Plugin',
+  columnCount: 3,
+  colWidth: 265,
+  theme: 'all',
+  themeExclude: ['default'],
+  style: {
+    fontColor: '#ceb78b',
+    descColor: '#eee',
+    contBgColor: 'rgba(6, 21, 31, .5)',
+    contBgBlur: 3,
+    headerBgColor: 'rgba(6, 21, 31, .4)',
+    rowBgColor1: 'rgba(6, 21, 31, .2)',
+    rowBgColor2: 'rgba(6, 21, 31, .35)'
+  },
+  bgBlur: false
+}
+
+export const bqbhelpList = [
+  {
+    group: '可不加#前缀',
+    list: [
+      {
+        icon: 88,
+        title: "#随机龙图",
+        desc: "攻击性有待提高"
+      },
+      {
+        icon: 88,
+        title: "#随机甘城猫猫",
+        desc: "来点Nacho！"
+      },
+      {
+        icon: 88,
+        title: "#随机fufu",
+        desc: "你就是歌姬吧！"
+      },
+      {
+        icon: 88,
+        title: "#随机丛雨",
+        desc: "Ciallo～(∠・ω< )⌒☆"
+      },
+      {
+        icon: 88,
+        title: "#随机小南梁",
+        desc: "被小南梁害惨了"
+      },
+      {
+        icon: 88,
+        title: "#随机千恋万花",
+        desc: "柚子厨蒸鹅心"
+      },
+      {
+        icon: 88,
+        title: "#随机古拉",
+        desc: "来点gura！"
+      },
+      {
+        icon: 88,
+        title: "#随机心海",
+        desc: "诶嘿嘿心海~"
+      },
+      {
+        icon: 88,
+        title: "#随机柴郡",
+        desc: "搞什么啊我只是猫咪"
+      },
+      {
+        icon: 88,
+        title: "#随机满穗",
+        desc: "参见万穗爷"
+      },
+      {
+        icon: 88,
+        title: "#随机猫猫虫",
+        desc: "咖波"
+      },
+      {
+        icon: 88,
+        title: "#随机纳西妲",
+        desc: "分享智慧"
+      },
+      {
+        icon: 88,
+        title: "#随机诗歌剧",
+        desc: "曼波！"
+      },
+      {
+        icon: 88,
+        title: "#随机kemomimi",
+        desc: "兽耳酱"
+      },
+    ]
+  }
 ]
 
 export const isSys = true
