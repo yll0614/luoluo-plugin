9e33962b6e06b9aafebef6f1baf58f079a314f51
叶落落 <2443958507@qq.com>
2024-08-12 10:06:14 +0000

    优化细节
    Signed-off-by: 叶落落 <2443958507@qq.com>

--- apps/Help/Helptheme.js
+++ apps/Help/Helptheme.js
@@ -1,6 +1,7 @@
 import lodash from 'lodash'
 import fs from 'fs'
 import { Data , Plugin_Name} from '../../components/index.js'
+
 let Theme = {
   async getThemeCfg (theme, exclude) {
     let dirPath = `./plugins/${Plugin_Name}/resources/help/theme/`
