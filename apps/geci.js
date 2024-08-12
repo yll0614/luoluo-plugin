336d087bbd37aa23fb1e8e2e5e3309d9c069ae2b
叶落落 <2443958507@qq.com>
2024-08-12 18:53:28 +0800

    优化细节

--- apps/geci.js
+++ apps/geci.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch"
 import fs from 'fs'
 import plugin from '../../../lib/plugins/plugin.js'
 import { Plugin_Name } from '../components/index.js'
-const clean =function clean(Lyrics) {
+const clean = function clean(Lyrics) {
     Lyrics = Lyrics.replace(/\[(.*?)\]/g, '');
     //Lyrics = Lyrics.replace(/\n/g, ''); //这个用来删除换行符
     Lyrics = Lyrics.replace(/ /g, '');//这个用来删除空格
@@ -28,15 +28,15 @@ export class geci extends plugin {
         const msg = e.msg.match(/^[#/]?(.*)歌词$/)[1]
         let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`)
         const API = JSON.parse(data)
-        let api = API.api1.url+ `?name=${msg}&format=lrc&n=1&br=5`
+        let api = API.api1.url + `?name=${msg}&format=lrc&n=1&br=2`
         let jx = await fetch(api)
         const Data = await (jx).json()
-        let codea = Data['code']
-        if (codea != '0') {
+        let code = Data['code']
+        if (code != '0') {
             e.reply([`A请求失败,请稍后再试或联系管理员!`])
             return true
         }
-        const  lyrics = Data['data']['lrc']['content']
+        const lyrics = Data['data']['lrc']['content']
         if (lyrics === undefined) {
             e.reply(['没有找到歌词数据，请确认歌曲名称是否正确。']);
             return true;
