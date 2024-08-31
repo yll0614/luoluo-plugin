6ef5464de6c0ff8df536113ddc98d880fe87883c
叶落落 <2443958507@qq.com>
2024-08-31 13:11:38 +0800

    优化细节

--- apps/tianqi.js
+++ apps/tianqi.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import fs from 'fs';
 import plugin from '../../../lib/plugins/plugin.js';
-import { Plugin_Name } from '../components/index.js';
+import { Plugin_Path } from '../components/index.js';
 
 export class tianqi extends plugin {
     constructor() {
@@ -20,7 +20,7 @@ export class tianqi extends plugin {
     }
     async cstq(e) {
         const msg = e.msg.match(/^[#/]?(.*)天气$/)[1];
-        let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`);
+        let data = await fs.readFileSync(`${Plugin_Path}/config/AllAPI.json`);
         const API = JSON.parse(data);
         let api = API.api6.url + `?city=${msg}`;
         let jx = await fetch(api);
