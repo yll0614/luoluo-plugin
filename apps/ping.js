fb09930f521352af123d2bcd553e4453015a9e44
叶落落 <2443958507@qq.com>
2024-08-31 13:11:01 +0800

    优化细节

--- apps/ping.js
+++ apps/ping.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch";
 import fs from 'fs';
 import plugin from '../../../lib/plugins/plugin.js';
-import { Plugin_Name } from '../components/index.js';
+import { Plugin_Path } from '../components/index.js';
 
 export class ping extends plugin {
     constructor() {
@@ -20,7 +20,7 @@ export class ping extends plugin {
     }
     async ping(e) {
         const msg = e.msg.match(/^[#/]?ping地址(.*)$/)[1];
-        let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`);
+        let data = await fs.readFileSync(`${Plugin_Path}/config/AllAPI.json`);
         const API = JSON.parse(data);
         let api = API.api7.url + `?format=json&ip=${msg}`;
         let jx = await fetch(api);
