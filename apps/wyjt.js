fd743032798c568ac10a40d62016eaef17a91f13
叶落落 <2443958507@qq.com>
2024-08-31 13:11:56 +0800

    优化细节

--- apps/wyjt.js
+++ apps/wyjt.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import plugin from '../../../lib/plugins/plugin.js'
-import { Plugin_Name } from '../components/index.js'
+import { Plugin_Path } from '../components/index.js'
 
 export class wyjt extends plugin {
     constructor() {
@@ -19,7 +19,7 @@ export class wyjt extends plugin {
     }
     async wyjt(e) {
         const msg = e.msg.match(/^[#/]?网页截图(.*)$/)[1]
-        let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`)
+        let data = await fs.readFileSync(`${Plugin_Path}/config/AllAPI.json`)
         const API = JSON.parse(data)
         let api = API.api9.url + `?url=${msg}`
         e.reply([segment.image(`${api}`)])
