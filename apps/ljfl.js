033b3692138bc53623623c99970ef355c536fd34
叶落落 <2443958507@qq.com>
2024-08-31 13:10:55 +0800

    优化细节

--- apps/ljfl.js
+++ apps/ljfl.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 import fs from 'fs'
 import plugin from '../../../lib/plugins/plugin.js'
-import { Plugin_Name } from '../components/index.js'
+import { Plugin_Path } from '../components/index.js'
 
 export class ljfl extends plugin {
     constructor() {
@@ -19,7 +19,7 @@ export class ljfl extends plugin {
     }
     async ljfl(e) {
         const msg = e.msg.match(/^[#/]?(.*)是什么垃圾$/)[1]
-        let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`)
+        let data = await fs.readFileSync(`${Plugin_Path}/config/AllAPI.json`)
         const API = JSON.parse(data)
         let api = API.api8.url + `?format=json&kw=${msg}`
         let jx = await fetch(api)
