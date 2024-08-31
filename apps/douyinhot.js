d770883ef3713e0f23b20d36a5909955cb25baa7
叶落落 <2443958507@qq.com>
2024-08-31 13:10:10 +0800

    优化细节

--- apps/douyinhot.js
+++ apps/douyinhot.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 import fs from 'fs'
 import plugin from '../../../lib/plugins/plugin.js'
-import { Plugin_Name } from '../components/index.js'
+import {Plugin_Path } from '../components/index.js'
 
 export class douyinhot extends plugin {
     constructor() {
@@ -21,7 +21,7 @@ export class douyinhot extends plugin {
 
 
     async douyinhot(e) {
-        let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`)
+        let data = await fs.readFileSync(`${Plugin_Path}/config/AllAPI.json`)
         const API = JSON.parse(data)
         let api = API.api16.url
         let jx = await fetch(api)
