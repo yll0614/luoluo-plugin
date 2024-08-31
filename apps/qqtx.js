c7bc8423195a84ba8582e2cc14c8bba366b3f715
叶落落 <2443958507@qq.com>
2024-08-31 13:11:21 +0800

    优化细节

--- apps/qqtx.js
+++ apps/qqtx.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import plugin from '../../../lib/plugins/plugin.js'
-import { Plugin_Name } from '../components/index.js'
+import { Plugin_Path } from '../components/index.js'
 
 export class qqtx extends plugin {
 
@@ -21,7 +21,7 @@ export class qqtx extends plugin {
     async qqtx(e) {
         let user_id = e.at || e.user_id
         user_id = Number(user_id) || String(user_id)
-        let data = await fs.readFileSync(`./plugins/${Plugin_Name}/config/AllAPI.json`)
+        let data = await fs.readFileSync(`${Plugin_Path}/config/AllAPI.json`)
         const API = JSON.parse(data)
         let api = API.api10.url + `?qq=${user_id}`
          e.reply([segment.image(`${api}`)])
