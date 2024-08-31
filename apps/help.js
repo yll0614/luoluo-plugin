a821f24507bc5df2979888ab4fc0dbd19ed16963
叶落落 <2443958507@qq.com>
2024-08-31 13:01:36 +0800

    优化细节

--- apps/help.js
+++ apps/help.js
@@ -17,7 +17,11 @@ export class luoluo_help extends plugin {
         },
         {
           reg: '^#?(ll|LL|Ll|lL|luoluo|落落|luoluo插件|ll插件|LL插件|Ll插件|lL插件|luoluo插件)表情包帮助$',
-          fnc : 'bqbhelp'
+          fnc: 'bqbhelp'
+        },
+        {
+          reg: '^#?(ll|LL|Ll|lL|luoluo|落落|luoluo插件|ll插件|LL插件|Ll插件|lL插件|luoluo插件)设置帮助$',
+          fnc: 'settinghelp'
         }
       ]
     });
@@ -27,6 +31,9 @@ export class luoluo_help extends plugin {
   }
   async bqbhelp() {
     return await help1(this.e)
+  }
+  async settinghelp() {
+    return await help2(this.e)
   }
 }
 async function help(e) {
@@ -94,3 +101,35 @@ async function help1(e) {
     element: 'default'
   }, { e, scale: 1 })
 }
+  async function help2(e) {
+    let custom = {}
+    let settinghelp = {}
+    let { diyCfg, sysCfg } = await Data.importCfg('help')
+    custom = settinghelp
+    let helpConfig = lodash.defaults(diyCfg.settinghelpCfg || {}, custom.settinghelpCfg, sysCfg.settinghelpCfg)
+    let helpList = diyCfg.settinghelpList || custom.settinghelpList || sysCfg.settinghelpList
+    let helpGroup = []
+    lodash.forEach(helpList, (group) => {
+      if (group.auth && group.auth === 'master' && !e.isMaster) {
+        return true
+      }
+      lodash.forEach(group.list, (help) => {
+        let icon = help.icon * 1
+        if (!icon) {
+          help.css = 'display:none'
+        } else {
+          let x = (icon - 1) % 10
+          let y = (icon - x - 1) / 10
+          help.css = `background-position:-${x * 50}px -${y * 50}px`
+        }
+      })
+      helpGroup.push(group)
+    })
+    let themeData = await Theme.getThemeData(diyCfg.settinghelpCfg || {}, sysCfg.settinghelpCfg || {})
+    return await render('help/index', {
+      helpCfg: helpConfig,
+      helpGroup,
+      ...themeData,
+      element: 'default'
+    }, { e, scale: 1 })
+  }
